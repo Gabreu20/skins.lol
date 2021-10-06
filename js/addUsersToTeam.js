de7d972f85cbcfa1321a50088a904e07af91e6fb
Gabreu20 <uplycomico@gmail.com>
2021-10-06 14:19:37 -0300

    belezas visuais

--- js/addUsersToTeam.js
+++ js/addUsersToTeam.js
@@ -1,14 +1,16 @@
 var userInTeam = [];
 function addUser() {
     userInTeam.push(document.getElementById('time').value);
-    var str = '<div class="usersT" id="' + document.getElementById('time').value + '">' + document.getElementById('time').value + '</div>';
+    var str = '<div onclick="deleteUser();" class="usersT" id="' + document.getElementById('time').value + '">' + document.getElementById('time').value + ' x ' + '</div>';
     document.getElementById("usersInTeam").insertAdjacentHTML('beforeend', str);
+    console.log(userInTeam);
 }
 function deleteUser(id) {
     const index = userInTeam.indexOf(id);
     if (index > -1) {
         userInTeam.splice(index, 1);
     }
+    console.log(userInTeam);
 }
 function match() {
     getUserSkins();
