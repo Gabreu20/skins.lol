3f7d8cf6fe2790701b92940bb8808ac1934af4c5
Gabreu20 <uplycomico@gmail.com>
2021-10-06 14:59:15 -0300

    Update addUsersToTeam.js

--- js/addUsersToTeam.js
+++ js/addUsersToTeam.js
@@ -1,11 +1,15 @@
 var userInTeam = [];
 function addUser() {
     userInTeam.push(document.getElementById('time').value);
-    var str = '<div onclick="deleteUser();" class="usersT" id="' + document.getElementById('time').value + '">' + document.getElementById('time').value + ' x ' + '</div>';
+    var str = '<div onclick="deleteUser(this.id);" class="usersT" id="' + document.getElementById('time').value + '">' + document.getElementById('time').value + ' x ' + '</div>';
     document.getElementById("usersInTeam").insertAdjacentHTML('beforeend', str);
     console.log(userInTeam);
 }
 function deleteUser(id) {
+    if (document.getElementById(id) !== null) {
+        var myDiv = document.getElementById(id);
+        myDiv.parentNode.removeChild(myDiv);
+    }
     const index = userInTeam.indexOf(id);
     if (index > -1) {
         userInTeam.splice(index, 1);
